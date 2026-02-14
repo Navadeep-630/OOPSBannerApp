001344e0f75e52c835b83785230c5cacf3639e71
Navadeep-630 <nk7269@srmist.edu.in>
2026-02-14 20:34:36 +0530

    Navadeep-630 Added: UC1 - Print OOPS banner

---
+++ OOPSBanner.java
@@ -0,0 +1,13 @@
+public class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to OOPSBanner application");
+        System.out.println("==== OOPS CONCEPTS ====");
+        System.out.println("1. Encapsulation");
+        System.out.println("2. Inheritance");
+        System.out.println("3. Polymorphism");
+        System.out.println("4. Abstraction");
+
+    }
+}
