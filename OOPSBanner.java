9348e7d1232dc999f668a146646e2425122a4f12
Navadeep-630 <nk7269@srmist.edu.in>
2026-02-17 13:09:47 +0530

    UC2 - OOPS Banner Display

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -3,6 +3,14 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
        
+        System.out.println(" ***    ***   ***   *****");
+        System.out.println("*   *  *   *  *  *  *    ");
+        System.out.println("*   *  *   *  ***   **** ");
+        System.out.println("*   *  *   *  *         *");
+        System.out.println("*   *  *   *  *         *");
+        System.out.println("*   *  *   *  *         *");
+        System.out.println(" ***    ***   *     *****");
+
 
         System.out.println(String.join(" ",
                 "  ***  ",
