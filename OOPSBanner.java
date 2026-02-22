9d4642ef2f2b54aac4c3b1b07d9a1624cb643516
Navadeep-630 <nk7269@srmist.edu.in>
2026-02-22 17:09:39 +0530

    UC3:Render OOPS as Banner using string.join()

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,14 +1,54 @@
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ * Using String.join() for better memory efficiency
+ */
+
 public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        System.out.println(" ***    ***   ***   *****");
-        System.out.println("*   *  *   *  *  *  *    ");
-        System.out.println("*   *  *   *  ***   **** ");
-        System.out.println("*   *  *   *  *         *");
-        System.out.println("*   *  *   *  *         *");
-        System.out.println("*   *  *   *  *         *");
-        System.out.println(" ***    ***   *     *****");
+        // Each line represents O, O, P, S
 
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " ******",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *    *",
+                " *     "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                "     * "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                "     * "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                " *    *"));
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " *     ",
+                " ***** "));
     }
 }
