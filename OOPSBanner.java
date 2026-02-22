230d30644caf5c28631a89b827f3035f92a9416f
Navadeep-630 <nk7269@srmist.edu.in>
2026-02-22 17:17:04 +0530

    Resolved merge conflict in OOPSBannerApp

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,16 +1,9 @@
-public class OOPSBannerApp {
+
+public class OOPSBanner {
 
     public static void main(String[] args) {
 
-       
-        System.out.println(" ***    ***   ***   *****");
-        System.out.println("*   *  *   *  *  *  *    ");
-        System.out.println("*   *  *   *  ***   **** ");
-        System.out.println("*   *  *   *  *         *");
-        System.out.println("*   *  *   *  *         *");
-        System.out.println("*   *  *   *  *         *");
-        System.out.println(" ***    ***   *     *****");
-
+   
 
         System.out.println(String.join(" ",
                 "  ***  ",
