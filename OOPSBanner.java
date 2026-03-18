a2e985e6d3f5d37a34abb1445f6b390a50207183
Navadeep-630 <nk7269@srmist.edu.in>
2026-03-18 12:59:05 +0530

    UC8 using MapCollection

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,16 +1,44 @@
+import java.util.*;
+
 public class OOPSBanner {
+
     public static void main(String[] args) {
 
-        String[] oops = {
-            "OOOO   OOOO   PPPP    SSSSS ",
-            "O   O  O   O  P   P  S      ",
-            "O   O  O   O  PPPP    SSSS   ",
-            "O   O  O   O  P           S  ",
-            "OOOO   OOOO   P      SSSSS   "
-        };
+        // Map to store patterns of each letter
+        Map<Character, String[]> map = new HashMap<>();
 
-        for (String line : oops) {
-            System.out.println(line);
+        map.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        map.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        map.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        String word = "OOPS";
+
+        // Print row by row
+        for (int i = 0; i < 5; i++) {
+            for (char ch : word.toCharArray()) {
+                System.out.print(map.get(ch)[i] + "  ");
+            }
+            System.out.println();
         }
     }
 }
